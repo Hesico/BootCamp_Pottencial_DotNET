97ff2805e2930b7becf8a846f23b575822dc252e
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2022-11-03 21:43:43 -0300

    Atualização do Exemplo MVC

--- Exemplos/MVC/Controllers/ContatoController.cs
+++ Exemplos/MVC/Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MVC.Context;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
@@ -18,6 +19,65 @@ namespace MVC.Controllers
         public IActionResult Index(){
             var contatos =  _context.Contatos.ToList();
             return View(contatos);
+        }
+
+        public IActionResult Criar(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato){
+            if(ModelState.IsValid){
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+        
+        public IActionResult Editar(int id)
+        {
+            var contato =  _context.Contatos.Find(id);
+            if(contato == null) return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+
+            _context.Contatos.Update(contato);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id){
+
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null) return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id){
+
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null) return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato){
+
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
